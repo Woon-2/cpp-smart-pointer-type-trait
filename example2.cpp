1deae3c05fea102dcdffd912508c95ea597b8186
MyeongWoonJang <73771162+MyeongWoonJang@users.noreply.github.com>
2021-12-21 22:55:19 +0900

    memory leak fixed (example2.cpp)

--- example2.cpp
+++ example2.cpp
@@ -15,22 +15,30 @@ public:
 	void reset( Ty* target = nullptr )
 	{
 		if constexpr ( is_smart_ptr_v< Ptr > ) impl.reset( target );
-		else impl = target;
+		else
+		{
+			if ( impl ) delete impl;
+			impl = target;
+		}
 	}
 
-	void release() { impl = nullptr; }
+	void release()
+	{
+		if constexpr ( std::is_pointer_v< Ptr > ) delete impl;
+		impl = nullptr;
+	}
 
 	Ty* get() noexcept { return operator->(); }
 	const Ty* get() const noexcept { return operator->(); }
 
 	decltype( auto ) get_deleter() noexcept
 	{
-		static_assert( is_smart_ptr_v< Ptr >, "pointer_impl< Ty, Ptr >::get_deleter(): Ptr was not a smart pointer." );
+		static_assert( is_unique_ptr_v< Ptr >, "pointer_impl< Ty, Ptr >::get_deleter(): Ptr was not a unique pointer." );
 		return impl.get_deleter();
 	}
 	decltype( auto ) get_deleter() const noexcept
 	{
-		static_assert( is_smart_ptr_v< Ptr >, "pointer_impl< Ty, Ptr >::get_deleter(): Ptr was not a smart pointer." );
+		static_assert( is_unique_ptr_v< Ptr >, "pointer_impl< Ty, Ptr >::get_deleter(): Ptr was not a unique pointer." );
 		return impl.get_deleter();
 	}
 
@@ -42,7 +50,7 @@ public:
 
 	// special member functions
 	pointer_impl( Ty* impl = nullptr ) : impl{ impl } {}
-	~pointer_impl() { impl = nullptr; }
+	~pointer_impl() { release(); }
 
 private:
 	Ptr impl;
@@ -104,7 +112,7 @@ int main()
 	std::cout << "========================================\n\n\n";
 
 	std::cout << "get_deleter() call =====================\n";
-	std::cout << typeid( a.get_deleter() ).name() << '\n';
+	std::cout << typeid( a.get_deleter() ).name() << '\n';			// only can executed with unique pointer
 	std::cout << "========================================\n\n\n";
 	
 	std::cout << "print value ============================\n";
