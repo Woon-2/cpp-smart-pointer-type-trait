d95decbc4adc6a67e84ac210f5272dbcbf0f4c94
MyeongWoonJang <73771162+MyeongWoonJang@users.noreply.github.com>
2021-12-21 20:50:53 +0900

    added is_ptr

--- example.cpp
+++ example.cpp
@@ -10,19 +10,21 @@ struct Sptr : std::shared_ptr< Ty >
 {
 	void func()
 	{
-		std::cout << "¿¹\n";
+		std::cout << "test\n";
 	}
 	Sptr( const Ty& val ) : std::shared_ptr< Ty >( new Ty{ val } ) {}
 };
+
 template < typename Ty >
 struct Uptr : std::unique_ptr< Ty >
 {
 	void func()
 	{
-		std::cout << "¿¹\n";
+		std::cout << "test\n";
 	}
 	Uptr( const Ty& val ) : std::unique_ptr< Ty >( new Ty{ val } ) {}
 };
+
 int main()
 {
 	auto check = []( auto&& target )
@@ -31,19 +33,29 @@ int main()
 		std::cout << "is_shared_ptr: " << is_shared_ptr_v< decltype( target ) > << '\n';
 		std::cout << "is_unique_ptr: " << is_unique_ptr_v< decltype( target ) > << '\n';
 		std::cout << "is_smart_ptr: " << is_smart_ptr_v< decltype( target ) > << '\n';
-		std::cout << "inherit_from_shared_ptr: " << inherit_from_shared_ptr_v< decltype( target ) > << '\n';
-		std::cout << "inherit_from_unique_ptr: " << inherit_from_unique_ptr_v< decltype( target ) > << '\n';
-		std::cout << "inherit_from_smart_ptr: " << inherit_from_smart_ptr_v< decltype( target ) > << '\n';
+		std::cout << "is_ptr: " << is_ptr_v< decltype( target ) > << '\n';
+		std::cout << "is_shared_ptr_soft: " << is_shared_ptr_soft_v< decltype( target ) > << '\n';
+		std::cout << "is_unique_ptr_soft: " << is_unique_ptr_soft_v< decltype( target ) > << '\n';
+		std::cout << "is_smart_ptr_soft: " << is_smart_ptr_soft_v< decltype( target ) > << '\n';
+		std::cout << "is_ptr_soft: " << is_ptr_soft_v< decltype( target ) > << '\n';
 		std::cout << "\n\n\n";
 	};
+
 	const auto a = std::make_unique< int >( 3 );
 	check( a );
+
 	Uptr< int > b{ 2 };
 	check( b );
+
 	auto c = std::make_shared< int >( 4 );
 	check( c );
+
 	Sptr< int > d{ 5 };
 	check( d );
+
 	int e = 8;
 	check( e );
+
+	int* f = &e;
+	check( f );
 }
--- smart_pointer_type_trait.hpp
+++ smart_pointer_type_trait.hpp
@@ -1,7 +1,8 @@
 // ==========================================================================
 // type traits for smart pointers.
-// supporting is_unique_ptr< T >, is_shared_ptr< T >, is_smart_pointer< T >.
+// supporting is_unique_ptr< T >, is_shared_ptr< T >, is_smart_ptr< T >, is_ptr< T >
 // usage is like std::is_pointer< T >.
+// 
 // ==========================================================================
 
 #ifndef _smart_pointer_type_trait
@@ -9,41 +10,105 @@
 
 #include <memory>
 
-template < typename T > struct is_shared_ptr_impl : std::false_type {};
-template < typename T > struct is_shared_ptr_impl< std::shared_ptr< T > > : std::true_type {};
-template < typename T > struct is_shared_ptr : is_shared_ptr_impl< std::remove_cv_t< std::remove_reference_t< T > > > {};
-template < typename T > constexpr bool is_shared_ptr_v = is_shared_ptr< T >::value;
-
-template < typename T > struct is_unique_ptr_impl : std::false_type {};
-template < typename T, typename Dx > struct is_unique_ptr_impl< std::unique_ptr< T, Dx > > : std::true_type {};
-template < typename T > struct is_unique_ptr : is_unique_ptr_impl< std::remove_cv_t< std::remove_reference_t< T > > > {};
-template < typename T > constexpr bool is_unique_ptr_v = is_unique_ptr< T >::value;
+// =======================================================================================
+// shared_ptr type trait
+// =======================================================================================
+template < typename T >
+struct is_shared_ptr_impl : std::false_type {};
 
 template < typename T >
-std::true_type inherit_from_shared_ptr_impl( const std::shared_ptr< T >* );
-std::false_type inherit_from_shared_ptr_impl( ... );
+struct is_shared_ptr_impl< std::shared_ptr< T > > : std::true_type {};
+
 template < typename T >
-using inherit_from_shared_ptr = decltype( inherit_from_shared_ptr_impl( std::declval< std::remove_cv_t< std::remove_reference_t< T > >* >() ) );
-template < typename T > constexpr bool inherit_from_shared_ptr_v = inherit_from_shared_ptr< T >::value;
+struct is_shared_ptr : is_shared_ptr_impl< std::remove_cv_t< std::remove_reference_t< T > > > {};
+
+template < typename T >
+constexpr bool is_shared_ptr_v = is_shared_ptr< T >::value;
+
+template < typename T >
+std::true_type is_shared_ptr_soft_impl( const std::shared_ptr< T >* );
+std::false_type is_shared_ptr_soft_impl( ... );
+
+template < typename T >
+using is_shared_ptr_soft = decltype( is_shared_ptr_soft_impl( std::declval< std::remove_cv_t< std::remove_reference_t< T > >* >() ) );
+
+template < typename T >
+constexpr bool is_shared_ptr_soft_v = is_shared_ptr_soft< T >::value;
+
+// shared_ptr type trait end ==============================================================
+
+// =======================================================================================
+// unique_ptr type trait
+// =======================================================================================
+template < typename T >
+struct is_unique_ptr_impl : std::false_type {};
 
 template < typename T, typename Dx >
-std::true_type inherit_from_unique_ptr_impl( const std::unique_ptr< T, Dx >* );
-std::false_type inherit_from_unique_ptr_impl( ... );
-template < typename T >
-using inherit_from_unique_ptr = decltype( inherit_from_unique_ptr_impl( std::declval< std::remove_cv_t< std::remove_reference_t< T > >* >() ) );
-template < typename T > constexpr bool inherit_from_unique_ptr_v = inherit_from_unique_ptr< T >::value;
+struct is_unique_ptr_impl< std::unique_ptr< T, Dx > > : std::true_type {};
 
-template < typename T > struct is_smart_ptr : std::conditional_t< is_shared_ptr_v< T >, std::true_type,
+template < typename T >
+struct is_unique_ptr : is_unique_ptr_impl< std::remove_cv_t< std::remove_reference_t< T > > > {};
+
+template < typename T > constexpr bool is_unique_ptr_v = is_unique_ptr< T >::value;
+
+template < typename T, typename Dx >
+std::true_type is_unique_ptr_soft_impl( std::unique_ptr< T, Dx >* );
+std::false_type is_unique_ptr_soft_impl( ... );
+
+template < typename T >
+using is_unique_ptr_soft = decltype( is_unique_ptr_soft_impl( std::declval< std::remove_cv_t< std::remove_reference_t< T > >* >() ) );
+
+template < typename T >
+constexpr bool is_unique_ptr_soft_v = is_unique_ptr_soft< T >::value;
+
+// unique_ptr type trait end ==============================================================
+
+// =======================================================================================
+// united type trait ( smart pointer, pointer( smart pointers + raw_pointer ) )
+// =======================================================================================
+template < typename T >
+struct is_smart_ptr : std::conditional_t< is_shared_ptr_v< T >, std::true_type,
 	std::conditional_t< is_unique_ptr_v< T >, std::true_type, std::false_type > > {};
+
 template < typename T > constexpr bool is_smart_ptr_v = is_smart_ptr< T >::value;
 
 template < typename T, typename Dx >
-std::true_type inherit_from_smart_ptr_impl( const std::unique_ptr< T, Dx >* );
+std::true_type is_smart_ptr_soft_impl( std::unique_ptr< T, Dx >* );
 template < typename T >
-std::true_type inherit_from_smart_ptr_impl( const std::shared_ptr< T >* );
-std::false_type inherit_from_smart_ptr_impl( ... );
+std::true_type is_smart_ptr_soft_impl( std::shared_ptr< T >* );
+std::false_type is_smart_ptr_soft_impl( ... );
+
 template < typename T >
-using inherit_from_smart_ptr = decltype( inherit_from_smart_ptr_impl( std::declval< std::remove_cv_t< std::remove_reference_t< T > >* >() ) );
-template < typename T > constexpr bool inherit_from_smart_ptr_v = inherit_from_smart_ptr< T >::value;
+using is_smart_ptr_soft = decltype( is_smart_ptr_soft_impl( std::declval< std::remove_cv_t< std::remove_reference_t< T > >* >() ) );
+
+template < typename T >
+constexpr bool is_smart_ptr_soft_v = is_smart_ptr_soft< T >::value;
+
+
+template < typename T >
+struct is_ptr_impl : std::conditional_t< std::is_pointer_v< T >, std::true_type,
+	std::conditional_t< is_smart_ptr_v< T >, std::true_type, std::false_type > > {};
+
+template < typename T >
+struct is_ptr : is_ptr_impl< std::remove_cv_t< std::remove_reference_t< T > > > {};
+
+template < typename T >
+constexpr bool is_ptr_v = is_ptr< T >::value;
+
+template < typename T, typename Dx >
+std::true_type is_ptr_soft_impl( std::unique_ptr< T, Dx >* );
+template < typename T >
+std::true_type is_ptr_soft_impl( std::shared_ptr< T >* );
+template < typename T >
+std::true_type is_ptr_soft_impl( T** );
+std::false_type is_ptr_soft_impl( ... );
+
+template < typename T >
+using is_ptr_soft = decltype( is_ptr_soft_impl( std::declval< std::remove_cv_t< std::remove_reference_t< T > >* >() ) );
+
+template < typename T >
+constexpr bool is_ptr_soft_v = is_ptr_soft< T >::value;
+
+// united type trait end ==============================================================
 
 #endif
